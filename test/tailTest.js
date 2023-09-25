5d0f40fb9dfd012600945e68fef9cb0c56df04ac
JP Saliba <jpsaliba@outlook.com>
2023-09-25 14:58:23 -0400

    More Mocha and Chai assert usage instead of the manual assertions made earlier

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,8 @@
-const assertEqual = require('../assertEqual')
-const eqArrays = require('../eqArrays')
+const assert = require('chai').assert;
 const tail = require('../tail')
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail! (unless you use eqArrays as well)
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
